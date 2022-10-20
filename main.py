80db0a687bdcdde168343fb6bb1bbebcf424807f
FlonEsUn <Past_uu72@student.itstep.org>
2022-10-20 17:55:55 +0300

    committ

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-a = int(input("Enter: "))
-print(a + a)
+# class Student:
+#     def __init__(self):
+#         self.height = 170
+#         print("Im alive!")
+#
+# nick = Student()
+# print(nick.height)
+
+# class Student:
+#     def __init__(self, name = yarik):
+#         self.name =  name
+
+class Square:
+    def __init__(self, side = 10):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+sq = Square
+print(self.sq)
